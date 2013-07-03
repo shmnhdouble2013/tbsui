43354abaefdc7bb4d13d0b2fc2c918dabe6d8a09
dafeng <dafeng.xdf@taobao.com>
2013-07-03 20:02:55 +0800

    add select

--- js/tbsui.js
+++ js/tbsui.js
@@ -3,9 +3,9 @@
  * @Author:      dafeng.xdf[at]taobao.com
  * @Date:        2013.7.3
  */
-KISSY.add('tbsui', function (S) {
+KISSY.add('tbsui', function (S,XTemplate) {
     'use strict';
-    var D = S.DOM;
+    var D = S.DOM,E = S.Event;
     /**
      * @name Tbsui
      * @class 模拟控件扁平化
@@ -14,6 +14,7 @@ KISSY.add('tbsui', function (S) {
     function Tbsui(cfg) {
         var self = this;
         self.selector = cfg.selector;
+        self.prifix = 'slct-block';
         self.init();
     }
 
@@ -27,19 +28,69 @@ KISSY.add('tbsui', function (S) {
             S.each(self.selector,function(i){
                 S.each(S.query(i),function(j){
                    if (j.nodeName === 'SELECT' && D.hasClass(j,'slct')){
-                    console.log(1)
+                       self.createSelect(j);
                    }else if(j.nodeName === 'RADIO' && D.hasClass(j,'huangjia')){
 
                    }
                 });
             });
         },
-        createSelect:function(){
-            var tpl = '';
+        createSelect:function(elm){
+            var self = this,
+                tpl = '<div class="'+self.prifix+'">' +
+                '<div class="clearfix title">' +
+                '<span class="option">{{optionName}}</span>' +
+                '<span class="icon"></span>' +
+                '</div>' +
+                '<ul class="menu">{{#each options}}' +
+                '<li data-index="{{xindex}}" class="">' +
+                '{{this}}' +
+                '</li>{{/each}}'+
+                '</ul></div>',
+                _options = [];
+            S.each(elm.options,function(i){
+                _options.push(i.text);
+            });
+            var data = {
+                    optionName: D.attr(elm,'data-name'),
+                    options: _options
+                },
+                render = new XTemplate(tpl).render(data);
+            D.insertAfter(D.create(render),elm);
+            var panel = D.get('.menu',D.next(elm));
+            E.delegate('.'+self.prifix,'click','div',function(e){
+                if(D.hasClass(e.target,'title')){
+                    D.toggle(panel);
+                }
+            });
+            E.delegate('.'+self.prifix,'click mouseenter mouseleave','li',function(e){
+                switch(e.type){
+                    case 'click':
+                        self.selectOption(elm,D.attr(e.target,'data-index'));
+                        D.hide(panel);
+                        break;
+                    case 'mouseenter':
+                        D.addClass(e.target,'selected');
+                        break;
+                    case 'mouseleave':
+                        D.removeClass(e.target,'selected');
+                        break;
+                }
+            });
+            E.delegate('.'+self.prifix,'click','span',function(e){
+                if(e.target.nodeName == 'SPAN'){
+                    D.toggle(panel);
+                }
+            });
+            E.delegate(document,'click','div',function(e){
+                if(!D.contains(D.next(elm),e.target)){
+                    D.hide(panel);
+                }
+            });
         },
-        bindEvent:function(){
-
+        selectOption:function(elm,index){
+            elm.options[index].selected = 'selected';
         }
     });
     return Tbsui;
-}, {requires: ['dom']});
+}, {requires: ['xtemplate','dom','event']});
