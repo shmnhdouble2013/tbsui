eded54c7348e5a05b9808f459163b356b7a2b55e
dafeng <dafeng.xdf@taobao.com>
2013-07-03 14:36:51 +0800

    add files

---
+++ js/init.js
@@ -0,0 +1,7 @@
+KISSY.ready(function(S){
+    S.use('tbsui',function(S,Tbsui){
+        new Tbsui({
+            selector:['select']
+        });
+    })
+});
---
+++ js/tbsui.js
@@ -0,0 +1,41 @@
+/**
+ * @Description: 模拟控件扁平化
+ * @Author:      dafeng.xdf[at]taobao.com
+ * @Date:        2013.7.3
+ */
+KISSY.add('tbsui', function (S) {
+    'use strict';
+    var D = S.DOM;
+    /**
+     * @name Tbsui
+     * @class 模拟控件扁平化
+     * @constructor
+     */
+    function Tbsui(cfg) {
+        var self = this;
+        self.selector = cfg.selector;
+        self.init();
+    }
+
+    S.augment(Tbsui, {
+        init:function(){
+            var self = this;
+            self.detecte();
+        },
+        detecte:function(){
+            var self = this;
+            S.each(self.selector,function(i){
+                S.each(S.query(i),function(j){
+                    console.log(D.hasClass(j,'slct'));
+                });
+            });
+        },
+        createSelect:function(){
+            var tpl = '';
+        },
+        bindEvent:function(){
+
+        }
+    });
+    return Tbsui;
+}, {requires: ['dom']});
