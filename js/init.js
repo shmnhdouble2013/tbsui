867f109dfd3992b69c0f98ae06d5e1a4f4f72920
dafeng <dafeng.xdf@taobao.com>
2013-07-04 20:02:28 +0800

    add radio

--- js/init.js
+++ js/init.js
@@ -1,7 +1,7 @@
 KISSY.ready(function(S){
     S.use('tbsui',function(S,Tbsui){
         new Tbsui({
-            selector:['select']
+            selector:['select','radio','checkbox']
         });
     })
 });
--- js/tbsui.js
+++ js/tbsui.js
@@ -25,14 +25,22 @@ KISSY.add('tbsui', function (S,XTemplate) {
         },
         detecte:function(){
             var self = this;
+
             S.each(self.selector,function(i){
+
                 S.each(S.query(i),function(j,_key){
                    if (j.nodeName === 'SELECT' && D.hasClass(j,'slct')){
                        self.createSelect(j,_key);
-                   }else if(j.nodeName === 'RADIO' && D.hasClass(j,'huangjia')){
-
                    }
                 });
+
+                // 判断初始化元素
+                switch(i){
+                    case 'radio' : self._renderRidoCheckbox('radio');
+                        break;
+                    case 'checkbox' : self._renderRidoCheckbox('checkbox');
+                        break;
+                }
             });
         },
         createSelect:function(elm,indexKey){
@@ -109,7 +117,154 @@ KISSY.add('tbsui', function (S,XTemplate) {
         selectOption:function(elm,index,title){
             elm.options[index].selected = 'selected';
             D.html(title,elm.options[index].text);
+        },
+        //初始化 checkbox radio
+        _renderRidoCheckbox: function(inputType){
+            var self = this;
+
+            // 设定radio：dom 容器结样式
+            self.labelSpanCls = '.radoBox-icons';
+            self.protyInptCls = '.radoBox-cls';
+            self.radioDisabTypeCls = '.cursor-disabled';
+
+            self._setGlobalRdChkls(inputType);
+
+            // 设定radio数组
+            self.radios = S.query('input[type="'+inputType+'"]');
+
+            // 根据原始radio数据 回显自定义radio样式
+            self._radioStyRender(self.radios);
+
+            // radio label事件监控
+            E.add('.j_radoBox', 'click', function(el){
+                el.halt(true);
+                self._radioClick(el);
+            });
+        },
+
+        // 设定 全局 radio checkbox样式
+        _setGlobalRdChkls: function(inputType){
+            var self = this;
+            // radio 和 checkbox 4中状态样式
+            if(inputType == 'radio') {
+                self.radioChecdTypeCls = '.radio-checked';
+                self.radioUnchecdTypeCls = '.radio-unchecked';
+                self.radioChecdDisabCls = '.raido-disabled-checked';
+                self.radioUnchecdDisabCls = '.raido-disabled-unchecked';
+            }else if (inputType == 'checkbox') {
+                self.radioChecdTypeCls = '.checkbox-checked';
+                self.radioUnchecdTypeCls = '.checkbox-unchecked';
+                self.radioChecdDisabCls = '.checkbox-disabled-checked';
+                self.radioUnchecdDisabCls = '.checkbox-disabled-unchecked';
+            };
+        },
+
+        // 遍历同类radio集合，取消radio checked
+        _cancelRadioChed: function(el){
+            var self = this,
+                nameStr = D.attr(el, 'name'),
+                groupRadios = S.query('input[name="'+nameStr +'"]');
+
+            S.each(groupRadios, function(el){
+                el.checked = '';
+                D.removeAttr(el, 'checked');
+                self._radioChkboxStyle(el);
+            });
+        },
+
+        // 公用方法：单击 radio 或者 checkbox 样式效果 函数
+        _radioClick: function(el){
+            var self = this,
+                protoRadio = D.children(el.currentTarget, self.protyInptCls)[0],
+                isdisable = protoRadio.disabled || D.attr(protoRadio, 'disabled'),
+                isChecked = protoRadio.checked || D.attr(protoRadio, 'checked'),
+                radioOrBoxStr = protoRadio.type || D.attr(protoRadio, 'type'),
+                radioOrBox = radioOrBoxStr == 'radio' ? true : false;
+
+            // 重新复制全局状态样式
+            self._setGlobalRdChkls(radioOrBoxStr);
+
+            // 如果是 禁用 则直接退出
+            if(isdisable){
+                return;
+            }
+
+            // 如果是radio
+            if(radioOrBox){
+
+                // 如果选中直接退出
+                if(isChecked){
+                    return;
+                }
+
+                // 遍历radio集合 取消同组全部选中状态
+                self._cancelRadioChed(protoRadio);
+            }
+
+            self._radioChkboxStyle(protoRadio, radioOrBox);
+        },
+
+
+        // 公用方法：radio/checkbox 回显 + 隐藏-原生元素, 显示 替换元素
+        _radioStyRender:function(radios){
+            var self = this;
+
+            S.each(radios, function(el){
+                self._radioChkboxStyle(el);
+                D.addClass(el, 'hidd-el');
+            });
+        },
+
+        // 公用方法：自定义radio 和 checkbox 样式及原生属性同步
+        _radioChkboxStyle: function(el, isClickRdio){
+            var self = this,
+                isdisable = el.disabled || D.attr(el, 'disabled'),
+                isChecked = el.checked || D.attr(el, 'checked'),
+                sefRadio = D.prev(el, 'span'),
+                labContainer = D.parent(el, 'label');
+
+            // 如果是radio, 否则是checkbox 取反赋值
+            if(isClickRdio){
+                if(isChecked){
+                    return;
+                }else{
+                    isChecked = !isChecked;
+                }
+            }else if(isClickRdio == false){
+                isChecked = !isChecked;
+            }
+
+
+            // 是否禁用  -- 及选中情况
+            if(isdisable){
+
+                el.disabled = 'disabled';
+                D.addClass(labContainer, self.radioDisabTypeCls);
+
+                if(isChecked){
+                    el.checked = 'checked';
+                    D.removeClass(sefRadio, self.radioUnchecdDisabCls);
+                    D.addClass(sefRadio, self.radioChecdDisabCls);
+                }else{
+                    el.checked = '';
+                    D.removeClass(sefRadio, self.radioChecdDisabCls);
+                    D.addClass(sefRadio, self.radioUnchecdDisabCls);
+                }
+            }else{
+                el.disabled = '';
+                D.removeClass(labContainer, self.radioDisabTypeCls);
+
+                if(isChecked){
+                    el.checked = 'checked';
+                    D.removeClass(sefRadio, self.radioUnchecdTypeCls);
+                    D.addClass(sefRadio, self.radioChecdTypeCls);
+                }else{
+                    el.checked = '';
+                    D.removeClass(sefRadio, self.radioChecdTypeCls);
+                    D.addClass(sefRadio, self.radioUnchecdTypeCls);
+                }
+            }
         }
     });
     return Tbsui;
-}, {requires: ['xtemplate','dom','event']});
+}, {requires: ['xtemplate','dom','event','sizzle']});
